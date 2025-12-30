391b942d2c1426f6ec2acb4bdc170a3da9f11a00
Shivaji Shill <agrojitshill@gmail.com>
2025-12-31 01:20:07 +0600

    Lesson 2: if-else conditions with full comments

---
+++ 02-Condition/MultipleConditions.js
@@ -0,0 +1,19 @@
+// Declare a variable 'marks' and assign value 75
+let marks = 75;
+
+// Start decision making
+// Check if marks are 80 or more
+if (marks >= 80) {
+    // If true, print top grade
+    console.log("A+");
+
+} else if (marks >= 32) {
+    // If first condition is false but marks are 32 or more
+    // Then student has passed
+    console.log("Passed");
+
+} else {
+    // If all above conditions are false
+    // Student has failed
+    console.log("Failed");
+}
---
+++ 02-Condition/if-else.js
@@ -0,0 +1,14 @@
+// Declare a variable "age" with value 20
+let age = 20;
+
+// Check if age is 18 or more
+if (age >= 18) {
+
+    // If condition is true, this line will execute
+    console.log("You are an adult");
+
+}else {
+
+    // If condition is false, this line will execute
+    console.log("You are not an adult");
+}
