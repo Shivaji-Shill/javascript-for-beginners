b348cad10fc4e187e87488bc6090a97af2c62aa8
Shivaji Shill <agrojitshill@gmail.com>
2025-12-31 00:19:02 +0600

    Lesson 1: JavaScript basics

---
+++ 01-basics/script.js
@@ -0,0 +1,17 @@
+// console.log() is used to print output to the terminal
+// Whatever is written inside will be displayed as output
+console.log("JavaScript lesson 1 running");
+
+// Declare a variabke using let
+// The variable 'name' stores a string value
+let name = "Agrojit";
+
+//Declare another variable
+// The variable 'age' stores a number value
+let age = 20;
+
+// Print the value stored in the 'name' variable
+console.log(name);
+
+// Print the value stored in the 'age' variable
+console.log(age);
